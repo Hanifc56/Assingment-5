636d6b18cf3e5350dedc3f2899aa321dc21b114c
Hanifc56 <abuhanif007sd@gmail.com>
2022-02-19 11:59:47 +0600

    error handling done

--- js/script.js
+++ js/script.js
@@ -1,27 +1,43 @@
-function totalCalculation() {
+function totalAmount(foodcost, rentcost, clothcost) {
+    if (foodcost || rentcost || clothcost < 0) {
+        return alert('Enter a positive number');
+    }
+    return foodcost + rentcost + clothcost;
 
+
+}
+
+function restMoney(totalMoney, totalCost) {
+    if (totalMoney < 0) {
+        return alert('Enter a positive number');
+    }
+    return totalMoney - totalCost;
 }
 document.getElementById('calculate-btn').addEventListener('click', function () {
     // total income
-    const incomeTotal = document.getElementById('total-income');
-    const incomeAmount = parseInt(incomeTotal.value);
-    // console.log(incomeAmount);
-    // food cost
-    const foodCost = document.getElementById('food-cost');
-    const foodCostTotal = parseInt(foodCost.value);
-    // rent cost
-    const rentCost = document.getElementById('rent-cost');
-    const rentCostTotal = parseInt(rentCost.value);
-    // clothes cost
-    const clothCost = document.getElementById('cloth-cost');
-    const clothCostTotal = parseInt(clothCost.value);
-    // total cost
-    const totalCost = foodCostTotal + rentCostTotal + clothCostTotal;
-    // rest balance
-    const restBalance = incomeAmount - totalCost;
-
+    const totalIncome = document.getElementById('total-cost');
+    const totalMoney = parseInt(totalIncome.value);
+    // total food
+    const totalFood = document.getElementById('food-cost');
+    const foodMoney = parseInt(totalFood.value);
+    // total rent
+    const totalRent = document.getElementById('rent-cost');
+    const rentMoney = parseInt(totalRent.value);
+    // total cloth
+    const totalCloth = document.getElementById('cloth-cost');
+    const clothMoney = parseInt(totalCloth.value);
+    // total Cost
+    // const totalCost = foodMoney + rentMoney + clothMoney;
+    const totalCost = totalAmount(foodMoney, rentMoney, clothMoney);
+    console.log(totalCost);
+    // rest Balance 
+    // const restBalance = totalMoney - totalCost;
+    const restBalance = restMoney(totalMoney, totalCost);
     console.log(restBalance);
+    // Update price
+    const updatePrice = document.getElementById('total-expence');
+    const expance = updatePrice.innerText;
+    console.log(expance);
 
 
 })
-
